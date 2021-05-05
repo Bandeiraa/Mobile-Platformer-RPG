7291f1979ab8e2c806f9d9df78dc0a0fc56a2377
Davi <fcodavibandeira@edu.unifor.br>
2021-05-05 15:13:03 -0300

    Updating inventory system(WIP)

--- Scripts/GUI/Intentory/Slot4.gd
+++ Scripts/GUI/Intentory/Slot4.gd
@@ -1,5 +1,7 @@
 extends TextureRect
 
+signal send_index
+
 var item_name = ""
 var item_amount = 0
 var slot_occuped = false
@@ -7,3 +9,7 @@ var slot_occuped = false
 func get_item_info(received_item_name):
 	item_name = received_item_name
 	slot_occuped = true
+
+
+func _on_Slot4_mouse_entered():
+	emit_signal("send_index", get_index())
--- Scripts/GUI/InventorySlotContainer.gd
+++ Scripts/GUI/InventorySlotContainer.gd
@@ -1,12 +1,19 @@
 extends GridContainer
 
+signal items_changed
+
 onready var default_texture = load("res://Sprites/UI/Empty_Inventory_Slot.png")
 onready var in_use_texture = load("res://Sprites/UI/Filled_Inventory_Slot.png")
 
 var busy_slots = 0
+var items: Array = [null, null, null, null, null, null, null, null]
+var item_index
 
 func _ready():
 	randomize()
+	for slot in get_children():
+		slot.connect("send_index", self, "get_item_index")
+		
 	set_texture()
 	
 	
@@ -28,6 +35,7 @@ func get_item(item):
 		new_texture.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
 		get_child(choosen_item).add_child(new_texture)
 		get_child(choosen_item).get_item_info(item)
+		items[choosen_item] = new_texture
 		set_texture()
 		busy_slots += 1
 		
@@ -36,5 +44,69 @@ func set_texture():
 	for slot in get_children():
 		if slot.get_children() == []:
 			slot.texture = default_texture
+			
 		else:
 			slot.texture = in_use_texture
+
+
+func get_item_index(index):
+	item_index = index
+	
+	
+func get_drag_data(_position):
+	var current_slot = get_child(item_index)
+	var current_slot_children = current_slot.get_children()
+	if current_slot_children != []:
+		var item = current_slot.get_child(0).texture
+		remove_item(item_index)
+		var data = {}
+		data.item = item
+		data.item_index = item_index
+		var drag_preview = TextureRect.new()
+		drag_preview.texture = data.item
+		set_drag_preview(drag_preview)
+		return data
+	
+	
+func can_drop_data(_position, data):
+	return data is Dictionary and data.has("item")
+	
+	
+func drop_data(_position, data):
+	var current_item = items[item_index]
+	pass
+	
+	
+func set_item(item_index, item):
+	var previous_item = items[item_index]
+	items[item_index] = item
+	emit_signal("items_changed", [item_index])
+	return previous_item
+	
+	
+func swap_items(item_index, target_item_index):
+	var target_item = items[target_item_index]
+	var item = items[item_index]
+	items[target_item_index] = item
+	items[item_index] = target_item
+	emit_signal("items_changed", [item_index, target_item_index])
+	
+	
+func remove_item(index):
+	if items != []:
+		var previous_item = items[index]
+		#print(previous_item)
+		items[index] = null
+		emit_signal("items_changed", index)
+		return previous_item
+		
+		
+func items_changed(index):
+	print(items)
+	get_children()[index].get_child(0).queue_free()
+	#set_texture()
+
+
+func _unhandled_input(event):
+	if event.is_action_released("ui_left_mouse_button"):
+		set_texture()
